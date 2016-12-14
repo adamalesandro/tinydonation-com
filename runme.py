0e0699210a6ed97a92b9c4c56e20c5cf1fb5743a
Adam Alesandro <adamalesandro@gmail.com>
2016-12-14 11:39:07 -0500

    Updates pre launch

--- runme.py
+++ runme.py
@@ -9,5 +9,12 @@ def index():
     return flask.render_template("index.html")
 
 
+@app.context_processor
+def inject_values():
+    return {
+        "kickstarter_url": "https://kickstarter.com/"
+    }
+
+
 if __name__ == "__main__":
     app.run("localhost", port=8888)
